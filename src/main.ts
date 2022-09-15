785008cf9d08544ee2d69aac05ab8819760bb52f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-15 12:51:29 +0630

    feat: init sass

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 const app = createApp(App)
 
