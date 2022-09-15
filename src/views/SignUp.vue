e19c5cd9f73b6b231d37b637e3d55edf588edcdc
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-16 04:14:21 +0630

    feat: add sign up form

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -21,7 +21,7 @@ const user = $ref<User>({
 <template>
   <div class="bg-indigo-600 mx-4 px-6 py-8 rounded-lg md:max-w-2xl md:mx-auto ">
     <h2 class="text-gray-50 text-xl font-medium mb-6 sm:mb-8 sm:text-2xl">Sign Up</h2>
-    <form class="flex flex-col gap-y-6">
+    <form class="flex flex-col gap-y-6 mb-6">
       <FormInput
           label="Email"
           type="text"
@@ -48,6 +48,12 @@ const user = $ref<User>({
         Submit
       </button>
     </form>
+
+    <!--    Already have an account -->
+    <div class="flex justify-center items-center gap-x-2 text-gray-50 text-sm tracking-wide">
+      <span>Already have an account? </span>
+      <router-link to="/" class="bg-indigo-700/90 px-4 py-2 rounded-lg hover:opacity-90">Login</router-link>
+    </div>
 
   </div>
 </template>
