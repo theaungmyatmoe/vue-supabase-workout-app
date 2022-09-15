c4a5452f4de944264b6c4f6ad8f5651e4429e82c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-16 03:29:45 +0630

    feat: add sign up form

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>
-    sign up
+  <div class="bg-indigo-600 mx-4 px-6 py-8 rounded-lg md:max-w-2xl md:mx-auto ">
+    <h2 class="text-gray-50 text-xl font-medium mb-6 sm:mb-8 sm:text-2xl">Sign Up</h2>
+    <form>
+      <div class="mb-6">
+        <label for="email" class="block mb-3 text-sm font-medium text-gray-50">Email</label>
+        <input
+            type="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-3 focus:ring-indigo-400 block w-full p-2.5"
+            placeholder="name@example.com"
+            required="">
+      </div>
+      <div class="mb-6">
+        <label for="email" class="block mb-3 text-sm font-medium text-gray-50">Email</label>
+        <input
+            type="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-3 focus:ring-indigo-400 block w-full p-2.5"
+            placeholder="name@example.com"
+            required="">
+      </div>
+      <div class="mb-6">
+        <label for="email" class="block mb-3 text-sm font-medium text-gray-50">Email</label>
+        <input
+            type="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-3 focus:ring-indigo-400 block w-full p-2.5"
+            placeholder="name@example.com"
+            required="">
+      </div>
+
+      <button type="submit"
+              class="mt-2 transition-all ease-out duration-300 w-full px-4 py-2.5 flex justify-center items-center text-gray-50 bg-indigo-700 rounded-lg hover:bg-indigo-900/50">
+        Submit
+      </button>
+    </form>
+
   </div>
 </template>
